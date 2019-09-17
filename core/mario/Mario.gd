9e1b44d8789959afa7cd6f2dc5733b9be1893e9d
jshlw <JulioHenrique31501234@hotmail.com>
2019-09-17 12:09:32 -0300

    Add physics handle

--- core/mario/Mario.gd
+++ core/mario/Mario.gd
@@ -6,7 +6,12 @@ export(float) var speed = 64
 
 
 # Private variables
+var gravity = 64
 var direction = Vector2()
+
+
+# Constants
+const GRAVITY_DIRECTION = Vector2(0, 1)
 
 
 # References
@@ -24,10 +29,22 @@ func _input(event):
 		pass
 
 
+func _physics_process(delta):
+	if move_and_collide(gravity * GRAVITY_DIRECTION * delta):
+		gravity = 64
+	else:
+		gravity += gravity * GRAVITY_DIRECTION.y * delta
+
+
 func _process(delta):
 	direction.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	
-	move_and_collide(direction * speed * delta)
+	if global_position.y < 40:
+		translate(direction * speed * delta)
+	else:
+		OS.alert("Game Over!")
+		set_process(false)
+		get_tree().quit()
 	
 	if direction.x == 0:
 		Sprite.animation = "idle"
