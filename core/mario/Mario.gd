5116829a6647de302d2ad7707aaabcbfe761994c
jshlw <JulioHenrique31501234@hotmail.com>
2019-09-17 17:31:53 -0300

    Add Gomba sprites

--- core/mario/Mario.gd
+++ core/mario/Mario.gd
@@ -3,15 +3,22 @@ extends KinematicBody2D
 
 # Public variables
 export(float) var speed = 64
+export(float) var jump_height = 64
 
 
 # Private variables
-var gravity = 64
+var speed_x = 0
+var speed_y = 0
+
+var gravity = MIN_GRAVITY
 var direction = Vector2()
+
+var jumping = false
 
 
 # Constants
-const GRAVITY_DIRECTION = Vector2(0, 1)
+const MIN_GRAVITY = 100.0
+const GRAVITY_DIRECTION = Vector2.DOWN
 
 
 # References
@@ -26,22 +33,35 @@ func _input(event):
 		Sprite.scale.x = 1
 		Sprite.animation = "walking"
 	elif event.is_action_pressed("jump"):
-		pass
+		jump()
+
+
+func jump():
+	if not jumping:
+		jumping = true
+		
+		
+		jumping = false
 
 
 func _physics_process(delta):
-	if move_and_collide(gravity * GRAVITY_DIRECTION * delta):
-		gravity = 64
-	else:
-		gravity += gravity * GRAVITY_DIRECTION.y * delta
+	var collision = move_and_collide(direction * speed * delta)
+	
+	if collision:
+		if collision.collider is preload("res://core/monster/Monster.gd"):
+			queue_free()
+	
+	if not jumping:
+		if move_and_collide(gravity * GRAVITY_DIRECTION * delta):
+			gravity = MIN_GRAVITY
+		else:
+			gravity += gravity * GRAVITY_DIRECTION.y * delta
 
 
 func _process(delta):
 	direction.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	
-	if global_position.y < 40:
-		translate(direction * speed * delta)
-	else:
+	if global_position.y > 40:
 		OS.alert("Game Over!")
 		set_process(false)
 		get_tree().quit()
