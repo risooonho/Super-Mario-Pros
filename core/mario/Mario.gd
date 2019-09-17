cee7e97750fc56351556662a939792abb0eb3846
jshlw <JulioHenrique31501234@hotmail.com>
2019-09-17 10:57:27 -0300

    Update Mario input

--- core/mario/Mario.gd
+++ core/mario/Mario.gd
@@ -3,11 +3,10 @@ extends KinematicBody2D
 
 # Private variables
 var direction = Vector2()
-var max_speed = 2
+var max_speed = 25
 
-
-func _input(event):
-	direction.x = event.is_action_pressed() - event.is_action_pressed()
 
 func _process(delta):
+	direction.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	
 	move_and_collide(direction * max_speed * delta)
