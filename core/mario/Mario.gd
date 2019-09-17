cf0ca0589d23014cad746d74d6530be876a760a3
jshlw <JulioHenrique31501234@hotmail.com>
2019-09-17 11:14:54 -0300

    Reset animation to idle when Mario isn't moving

--- core/mario/Mario.gd
+++ core/mario/Mario.gd
@@ -13,11 +13,16 @@ onready var Sprite = $Sprite
 func _input(event):
 	if event.is_action_pressed("move_left"):
 		Sprite.scale.x = -1
+		Sprite.animation = "walking"
 	elif event.is_action_pressed("move_right"):
 		Sprite.scale.x = 1
+		Sprite.animation = "walking"
 
 
 func _process(delta):
 	direction.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	
 	move_and_collide(direction * max_speed * delta)
+	
+	if direction.x == 0:
+		Sprite.animation = "idle"
