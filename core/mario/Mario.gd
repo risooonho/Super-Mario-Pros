49c4eab722c0ba7854e75378fc9d258d06c35d93
jshlw <JulioHenrique31501234@hotmail.com>
2019-09-17 11:03:37 -0300

    Add sprite X invertion

--- core/mario/Mario.gd
+++ core/mario/Mario.gd
@@ -6,6 +6,17 @@ var direction = Vector2()
 var max_speed = 25
 
 
+# References
+onready var Sprite = $Sprite
+
+
+func _input(event):
+	if event.is_action_pressed("move_left"):
+		Sprite.scale.x = -1
+	elif event.is_action_pressed("move_right"):
+		Sprite.scale.x = 1
+
+
 func _process(delta):
 	direction.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	
