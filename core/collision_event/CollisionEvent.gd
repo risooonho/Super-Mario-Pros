48aed5acead0467fd814b6c1070f382fae01e360
jshlw <JulioHenrique31501234@hotmail.com>
2019-09-20 08:42:13 -0300

    Add CollisionEvent class

---
+++ core/collision_event/CollisionEvent.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+
+# Signals
+signal event_triggered
+
+
+# Private variables
+var ColliderObject
+var enterBody
+var initialized = false
+
+func initialize(_colliderObject, _enterBody):
+	self.ColliderObject = _colliderObject
+	self.enterBody = _enterBody
+	
+	if enterBody == true:
+		self.connect("body_entered", self, "_on_CollisionEvent_body_entered")
+	else:
+		self.connect("body_exited", self, "_on_CollisionEvent_body_exited")
+	
+	self.initialized = true
+
+
+func trigger_event():
+	emit_signal("event_triggered")
+
+
+func _on_CollisionEvent_body_entered(body):
+	if body == ColliderObject:
+		trigger_event()
+
+
+func _on_CollisionEvent_body_exited(body):
+	if body == ColliderObject:
+		trigger_event()
