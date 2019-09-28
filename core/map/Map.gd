bc039146bd10b7f91274e11f82c4d89cd7f04bea
BlackBearGames <33421921+jshlw@users.noreply.github.com>
2019-09-28 17:16:58 -0300

    Register class_name & dependency injection

--- core/map/Map.gd
+++ core/map/Map.gd
@@ -1,8 +1,13 @@
 extends Node
+class_name Map
 
 
-# References
-onready var KillAudio = $KillAudio
+# Public variables
+export(NodePath) var KillAudio_path
+
+
+# Private variables
+onready var KillAudio = get_node(KillAudio_path)
 
 
 func _handle_died(monster_position):
