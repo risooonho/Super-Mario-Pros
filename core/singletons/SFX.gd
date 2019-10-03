2b96a42ed2176409ff82fd52e4f69f4b816780a0
BlackBearGames <33421921+jshlw@users.noreply.github.com>
2019-10-03 15:07:53 -0300

    Add Sound class

--- core/singletons/SFX.gd
+++ core/singletons/SFX.gd
@@ -1,5 +1,10 @@
 extends Node
 
 
-func play():
-    pass
+var Sound = preload("res://core/sound/Sound.tscn")
+
+
+func play(sound_path):
+	var sound = Sound.instance()
+	sound.stream = load(sound_path)
+	add_child(sound)
