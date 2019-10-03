daaad812e3c0be6f641d09866be077e2c0de993d
jshlw <JulioHenrique31501234@hotmail.com>
2019-10-03 08:50:04 -0300

    Add coin class

--- core/CollectableItem/CollectableItem.gd
+++ core/CollectableItem/CollectableItem.gd
@@ -2,5 +2,17 @@ extends Area2D
 class_name CollectableItem
 
 
-func _ready():
-	pass
+# Signals
+signal collected
+
+
+# Public variables
+export(NodePath) var Map_path
+
+onready var Map = get_node(Map_path)
+
+
+func _on_CollectableItem_body_entered(body):
+	if body is Mario:
+		emit_signal("collected")
+		self.queue_free()
