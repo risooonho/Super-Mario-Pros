115ac897d76ab9ad878095608009691ce7355352
BlackBearGames <33421921+jshlw@users.noreply.github.com>
2019-10-03 15:08:46 -0300

    Update SFX.gd

--- core/singletons/SFX.gd
+++ core/singletons/SFX.gd
@@ -4,7 +4,8 @@ extends Node
 var Sound = preload("res://core/sound/Sound.tscn")
 
 
-func play(sound_path):
+func play(sound_path, volume_db):
 	var sound = Sound.instance()
 	sound.stream = load(sound_path)
+	sound.volume_db = volume_db
 	add_child(sound)
